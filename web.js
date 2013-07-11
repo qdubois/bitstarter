c5d3627dd3ea45f330f1f227b580527da1bf0ccf
Ubuntu <ubuntu@ip-172-31-33-217.us-west-2.compute.internal>
2013-07-11 02:44:46 +0000

    Fix web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var content;
-    fs.readFile('Index.html', function read(err, data) {
+    fs.readFile('index.html', function read(err, data) {
         if (err) {
             throw err;
         }
